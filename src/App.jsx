88ac3d45bdfef41c99e6a7d4ccb5c020fa85b855
Picodes10 <biswalsushree000@gmail.com>
2024-09-24 23:50:11 +0530

    Modified

--- src/App.jsx
+++ src/App.jsx
@@ -1,23 +1,14 @@
 // eslint-disable-next-line no-unused-vars
-import React, { useState, useEffect } from 'react';
-import Fetch from 'react-fetch';
-import City from './components/City';
-import Forecast from './components/WeatherForecast';
-
-const API_KEY = 'YOUR_API_KEY_HERE';
+import React from 'react'
+import './App.css'
+import Search from './components/search/search'
 
 const App = () => {
-
-  const [weatherData, setWeatherData] = useState(null);
-  const [error, setError] = useState('');
-
-  
   return (
-    <div className="app-container">
-      <City />
-      <Forecast />
+    <div className="container">
+      <Search />
     </div>
   )
 }
 
-export default App
+export default App 
--- src/components/city.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-const city = () => {
-  return (
-    <div>city</div>
-  )
-}
-
-export default city
